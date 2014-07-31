d7fa531e16a60bd1d20a2e6ff06bd4feff9370ff
Mason <hbcltj@gmail.com>
2014-07-31 14:01:32 +0800

    把无用的文件清除掉

--- main.py
+++
@@ -1,9 +0,0 @@
-#encoding=utf-8
-import sys
-sys.path.append('/home/Administrator/miniSpider/parse')
-sys.path.append('/home/Administrator/miniSpider/crawl')
-import parser
-import crawl
-
-if __name__ == "__main__":
-
